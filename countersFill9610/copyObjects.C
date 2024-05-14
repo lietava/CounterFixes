127cf2dca337f78d64d343e25e6e8fa68d22b2a2
lietava <roman.lietava@cern.ch>
2024-05-14 12:58:44 +0200

    clang

--- countersFill9610/copyObjects.C
+++ countersFill9610/copyObjects.C
@@ -9,7 +9,7 @@
 #include <map>
 #include <iostream>
 #endif
-void copyObjects(bool cfg =0, bool scl = 0, bool writeToCCDB = false ) {
+void copyObjects(bool cfg = 0, bool scl = 0, bool writeToCCDB = false) {
   std::vector<int> runNbVect = {551394,551398};
   const std::string ccdbScalers = "CTP/Calib/Scalers";
   const std::string ccdbConfig = "CTP/Config/Config";
@@ -35,48 +35,56 @@ void copyObjects(bool cfg =0, bool scl = 0, bool writeToCCDB = false ) {
     long tstart = pp.first + 60 * 1000;
     std::cout << "Run start:" << tstart << std::endl;
     apiCCDB.setURL("http://ccdb-test.cern.ch:8080");
-    if(scl) {
-      auto scl = apiCCDB.getSpecific<o2::ctp::CTPRunScalers>(ccdbScalers, tstart, metadata);
+    if (scl) {
+      auto scl = apiCCDB.getSpecific<o2::ctp::CTPRunScalers>(ccdbScalers,
+                                                             tstart, metadata);
       // validity of original files
-      auto headersS = apiTest.retrieveHeaders(ccdbScalers, metadata, tstart + 2000);
+      auto headersS =
+          apiTest.retrieveHeaders(ccdbScalers, metadata, tstart + 2000);
       const auto valSF = headersS.find("Valid-From");
       const auto valSU = headersS.find("Valid-Until");
       long valF = std::stol(valSF->second);
       long valU = std::stol(valSU->second);
       // write to ccdb
-      if(writeToCCDB) {
-        std::cout << "Scalers writing to CCDB startValGRP:" << valF << " endValGRP:" << valU << std::endl;
+      if (writeToCCDB) {
+        std::cout << "Scalers writing to CCDB startValGRP:" << valF
+                  << " endValGRP:" << valU << std::endl;
         apiProd.storeAsTFileAny(scl, ccdbScalers, metadata, valF, valU);
         isclw++;
       } else {
-        std::cout << "Scalers test (not writing) startValGRP:" << valF << " endValGRP:" << valU << std::endl;
+        std::cout << "Scalers test (not writing) startValGRP:" << valF
+                  << " endValGRP:" << valU << std::endl;
         isclt++;
       }
     }
     //
-    if(cfg) {
-      auto cfg = apiCCDB.getSpecific<o2::ctp::CTPConfiguration>(ccdbConfig, tstart, metadata);
+    if (cfg) {
+      auto cfg = apiCCDB.getSpecific<o2::ctp::CTPConfiguration>(
+          ccdbConfig, tstart, metadata);
       // validity of original files
-      auto headersS = apiTest.retrieveHeaders(ccdbConfig, metadata, tstart + 2000);
+      auto headersS =
+          apiTest.retrieveHeaders(ccdbConfig, metadata, tstart + 2000);
       const auto valSF = headersS.find("Valid-From");
       const auto valSU = headersS.find("Valid-Until");
       long valF = std::stol(valSF->second);
       long valU = std::stol(valSU->second);
       // write to ccdb
-      if(writeToCCDB) {
-        std::cout << "Config writing to CCDB startValGRP:" << valF << " endValGRP:" << valU << std::endl;
+      if (writeToCCDB) {
+        std::cout << "Config writing to CCDB startValGRP:" << valF
+                  << " endValGRP:" << valU << std::endl;
         apiProd.storeAsTFileAny(cfg, ccdbConfig, metadata, valF, valU);
         icfgw++;
       } else {
-        std::cout << "Config test (not writing) startValGRP:" << valF << " endValGRP:" << valU << std::endl;
+        std::cout << "Config test (not writing) startValGRP:" << valF
+                  << " endValGRP:" << valU << std::endl;
         icfgt++;
       }
     }
   }
   std::cout << "Number of runs in the list:" << runNbVect.size() << std::endl;
-  std::cout << "Number of scalers only tested (NOT written to ccdb):" << isclt << " written to CCDB:" << isclw << std::endl;
-  std::cout << "Number of configs only tested (NOT written to ccdb):" << icfgt << " written to CCDB:" << icfgw << std::endl;
+  std::cout << "Number of scalers only tested (NOT written to ccdb):" << isclt
+            << " written to CCDB:" << isclw << std::endl;
+  std::cout << "Number of configs only tested (NOT written to ccdb):" << icfgt
+            << " written to CCDB:" << icfgw << std::endl;
   return;
 }
-
-
